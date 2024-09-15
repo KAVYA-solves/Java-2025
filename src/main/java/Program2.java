c8dc55d20caf33a5fccb0441f4c4c1df8650f10b
KAVYA-solves <kavyaaleti68@gmail.com>
2024-09-15 10:16:51 +0000

    Program2

--- src/main/java/Program2.java
+++ src/main/java/Program2.java
@@ -4,7 +4,7 @@ public class Program2 {
 
     public static void main(String[] args) {
     
-        System.out.println("Hello world! from Program 2");
+        System.out.println("Hello world! from Program 2 ");
 
   }
 
