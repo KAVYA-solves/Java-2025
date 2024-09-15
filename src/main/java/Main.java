c187679a80954773168e6d3cf410a018dee91e65
KAVYA-solves <kavyaaleti68@gmail.com>
2024-09-15 09:52:55 +0000

    Sub programs for 2024 academic year

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,11 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!3");
+
+    // Create an instance of Program1
+    Program1 program1 = new Program1();
+    // Call the main method of Program1
+    program1.main(args); 
   }
 
   // @Test
---
+++ src/main/java/Program1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Program1 {
+
+    public static void main(String[] args) {
+    
+        System.out.println("Hello world! from Program 1");
+
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+
+}
